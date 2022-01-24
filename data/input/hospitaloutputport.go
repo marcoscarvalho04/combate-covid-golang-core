97feccdbecafd2fe3727d587d523a5fa54ed4bc2
Marcos <siqueira.marcos.04@gmail.com>
2022-01-24 19:43:27 -0300

    add outputport

---
+++ data/input/hospitaloutputport.go
@@ -0,0 +1,25 @@
+package input
+
+type HospitalOutputPort struct {
+	Id          int32  `json:"id"`
+	Nome        string `json:"nome"`
+	Cnpj        string `json:"cnpj"`
+	Endereco    string `json:"endereco"`
+	Localizacao struct {
+		Latitude  string `json:"latitude"`
+		Longitude string `json:"longitude"`
+	}
+	PercentualDeOcupacao float32 `json:"percentualDeOcupacao"`
+}
+
+func ConverterInputPortToOutputPort(hospitalInputPort HospitalInputPort) HospitalOutputPort {
+	hospitalOutputPort := HospitalOutputPort{}
+
+	hospitalOutputPort.Nome = hospitalInputPort.Nome
+	hospitalOutputPort.Cnpj = hospitalInputPort.Cnpj
+	hospitalOutputPort.Endereco = hospitalInputPort.Endereco
+	hospitalOutputPort.Localizacao = hospitalInputPort.Localizacao
+	hospitalOutputPort.PercentualDeOcupacao = hospitalInputPort.PercentualDeOcupacao
+
+	return hospitalOutputPort
+}
