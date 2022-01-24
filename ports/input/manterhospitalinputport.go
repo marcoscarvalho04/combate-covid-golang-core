a0dee2a4b3912f4d023f632ac402ccb24b4c4513
Marcos <siqueira.marcos.04@gmail.com>
2022-01-24 17:00:20 -0300

    fix salvarHospital struct

--- ports/input/manterhospitalinputport.go
+++ ports/input/manterhospitalinputport.go
@@ -2,14 +2,6 @@ package input
 
 import "github.com/marcoscarvalho04/combate-covid-golang-core/data/input"
 
-type HospitalOperations interface {
-	SalvarHospital(input.HospitalInputPort)
-}
-
-type HospitalInputPort struct {
-	Operations HospitalOperations
-}
-
-func (c *HospitalInputPort) SalvarHospital(input input.HospitalInputPort) {
+func SalvarHospital(input input.HospitalInputPort) {
 
 }
