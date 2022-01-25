09a4fd3116140159c0a9c58b6985252c887eb032
Marcos <siqueira.marcos.04@gmail.com>
2022-01-24 22:15:08 -0300

    implements salvarHospital contract

--- data/output/hospitaloutputport.go
+++ data/output/hospitaloutputport.go
@@ -12,16 +12,19 @@ type HospitalOutputPort struct {
 		Longitude string `json:"longitude"`
 	}
 	PercentualDeOcupacao float32 `json:"percentualDeOcupacao"`
+
+	Salvar SalvarHospital
+}
+type SalvarHospital interface {
+	SalvarHospital(outputPort HospitalOutputPort) (HospitalOutputPort, error)
 }
 
-func ConverterInputPortToOutputPort(hospitalInputPort input.HospitalInputPort) HospitalOutputPort {
-	hospitalOutputPort := HospitalOutputPort{}
-
-	hospitalOutputPort.Nome = hospitalInputPort.Nome
-	hospitalOutputPort.Cnpj = hospitalInputPort.Cnpj
-	hospitalOutputPort.Endereco = hospitalInputPort.Endereco
-	hospitalOutputPort.Localizacao = hospitalInputPort.Localizacao
-	hospitalOutputPort.PercentualDeOcupacao = hospitalInputPort.PercentualDeOcupacao
-
-	return hospitalOutputPort
+func (c *HospitalOutputPort) ConverterInputPortToOutputPort(hospitalInputPort input.HospitalInputPort) HospitalOutputPort {
+	return HospitalOutputPort{
+		Nome:                 hospitalInputPort.Nome,
+		Cnpj:                 hospitalInputPort.Cnpj,
+		Endereco:             hospitalInputPort.Endereco,
+		PercentualDeOcupacao: hospitalInputPort.PercentualDeOcupacao,
+		Localizacao:          hospitalInputPort.Localizacao,
+	}
 }
--- ports/input/manterhospitalinputport.go
+++ ports/input/manterhospitalinputport.go
@@ -1,7 +1,13 @@
 package input
 
-import "github.com/marcoscarvalho04/combate-covid-golang-core/data/input"
+import (
+	"github.com/marcoscarvalho04/combate-covid-golang-core/data/input"
+	"github.com/marcoscarvalho04/combate-covid-golang-core/data/output"
+)
 
-func SalvarHospital(input input.HospitalInputPort) {
+func SalvarHospital(inputPort input.HospitalInputPort) (output.HospitalOutputPort, error) {
+	output := output.HospitalOutputPort{}
+	outputPortData := output.ConverterInputPortToOutputPort(inputPort)
+	return output.Salvar.SalvarHospital(outputPortData)
 
 }
